a19f89c234938eb46ee64bf0af64e8fa5c8fc2c8
Tobias Landelius <tola@netcompany.com>
2021-05-09 21:32:31 +0200

    Remove footer

--- src/main/resources/db/migration/V1_4__text.sql
+++ src/main/resources/db/migration/V1_4__text.sql
@@ -5,3 +5,6 @@ TEXT_KEY VARCHAR(50) not null,
 TEXT TEXT not null,
 PRIMARY KEY (ID))
 ;
+INSERT INTO PULI_DB.TEXT (TEXT_GROUP, TEXT_KEY, TEXT) VALUES ('contact', 'name', 'Carina Karlsson');
+INSERT INTO PULI_DB.TEXT (TEXT_GROUP, TEXT_KEY, TEXT) VALUES ('contact', 'number', '+46(0)708 48 75 02');
+INSERT INTO PULI_DB.TEXT (TEXT_GROUP, TEXT_KEY, TEXT) VALUES ('contact', 'address', 'Slågarpsvägen 264-0 <br>Trelleborg - Sverige');
