bfe6dfdae41e09af27a791e8cc3eae7f3626cd0a
Tobias Landelius <tola@netcompany.com>
2021-11-18 19:24:39 +0100

    AddCorsMapping

--- src/main/java/puli/xaidaz/config/WebConfig.java
+++ src/main/java/puli/xaidaz/config/WebConfig.java
@@ -2,6 +2,7 @@ package puli.xaidaz.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
@@ -12,7 +13,7 @@ import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
-public class ResourceConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
 
     @Value("${environment}")
     private String environment;
@@ -45,5 +46,10 @@ public class ResourceConfig implements WebMvcConfigurer {
         }
         vr.setSuffix(".jsp");
         registry.viewResolver(vr);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 }
