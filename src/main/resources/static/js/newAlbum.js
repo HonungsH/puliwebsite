ff29ce748f4e07cdd8648db723a4740ec2adb6df
Hanne Heingård <hahe@netcompany.com>
2021-11-21 19:30:00 +0100

    no message

--- src/main/resources/static/js/newAlbum.js
+++ src/main/resources/static/js/newAlbum.js
@@ -7,7 +7,7 @@ window.onload = function () {
 $(document).ready(function(){
 
     $('#newAlbumSubmitButton').on('click', function() {// TODO felhantering
-        var title = $('#albumTitleInput').val();
+        const title = $('#albumTitleInput').val();
         $('#albumTitleHidden').val(title);
         $('#albumTitle').html(title);
         $('#newAlbumModal').modal('hide');
