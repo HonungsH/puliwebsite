d734c2b85d089909c7e598e92187825fabd55f36
Hanne Heingård <hahe@netcompany.com>
2021-11-22 19:32:34 +0100

    Script

--- src/main/resources/db/migration/V1_6__news.sql
+++ src/main/resources/db/migration/V1_6__news.sql
@@ -7,5 +7,6 @@ MODIFIED_AT DATETIME(6) NOT NULL,
 PROFILE_PICTURE VARCHAR(255),
 PRIMARY KEY (ID));
 INSERT INTO PuliDb.News (TITLE, TEXT, CREATED_AT, MODIFIED_AT, PROFILE_PICTURE) VALUES ('Valpar till påsk', '8 valpar efter Harry och Zava föddes den 1a april 2021. Tre vita, en tik och två hanar samt fem svarta.', '2021-04-01', '2021-04-01', '/images/dogs/all_puppies.jpg');
-INSERT INTO PuliDb.News (TITLE, TEXT, CREATED_AT, MODIFIED_AT, PROFILE_PICTURE) VALUES ('Alla valpar är nu hemma hos sina nya familjer!', 'Valparna efter Zava och Harry har alla hittat nya hem. Vi önskar alla stort lycka till med era nya familjemedlemmar. ', '2021-06-20', '2021-06-20', '/images/dogs/3_puppies.jpg');
+INSERT INTO PuliDb.News (TITLE, TEXT, CREATED_AT, MODIFIED_AT, PROFILE_PICTURE) VALUES ('Alla valpar är nu hemma hos sina nya familjer!', 'Test test test TEXT ?!=!=?!??+ ', '2021-06-20', '2021-06-20', '/images/dogs/3_puppies.jpg');
 INSERT INTO PuliDb.News (TITLE, TEXT, CREATED_AT, MODIFIED_AT, PROFILE_PICTURE) VALUES ('Valpar', 'Fyra valpar efter Doriz och Atilla föddes den 30 juli 2021. Alla svarta.', '2021-07-30', '2021-07-30', '/images/dogs/puppy_atilla_doriz.jpg');
+INSERT INTO PuliDb.News (TITLE, TEXT, CREATED_AT, MODIFIED_AT, PROFILE_PICTURE) VALUES ('Valpar förväntas till nyåar', 'En Nyhet utan Bild!', '2021-11-05', '2021-11-06', null);
